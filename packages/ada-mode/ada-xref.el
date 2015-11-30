fff2baef629c711211d4c653a5ddb2471ac18a26
Stefan Monnier <monnier@iro.umontreal.ca>
2015-11-30 13:31:14 -0500

    * packages/seq: Don't define it as a :core package
    Revert the removal of packages/seq/seq.el since it's different from
    the one in lisp/emacs-lisp.
    * .gitignore: Remove packages/seq.
    * externals-list: Remove "seq" entry.

--- packages/ada-mode/ada-xref.el
+++
@@ -1,4 +0,0 @@
-;;; ada-xref.el --- Dummy file to hide obsolete bundled version  -*- lexical-binding:t -*-
-(require 'ada-mode)
-(provide 'ada-xref)
-(message "'ada-xref' is obsolete; use 'ada-mode' instead")
