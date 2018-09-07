liudonghua123/elpa

commit 240f88cb4e317cb36bef7c929603c411bb4f104e

Don't warn on implementation-specific notifications (#93)
Only warn when method name doesn't start with '$'. Per the spec:

  "if a server or client receives notifications or requests 
  starting with ‘$/’ it is free to ignore them if they are unknown."

* eglot.el (eglot-handle-notification t t): Check method name for $.

eglot.el
@@ -987,7 +987,8 @@ Uses THING, FACE, DEFS and PREPEND."
 (cl-defmethod eglot-handle-notification
   (_server method &key &allow-other-keys)
   "Handle unknown notification"
-  (eglot--warn "Server sent unknown notification method `%s'" method))
+  (unless (string-prefix-p "$" method)
+    (eglot--warn "Server sent unknown notification method `%s'" method)))
 
 (cl-defmethod eglot-handle-request
   (_server method &key &allow-other-keys)

commit 0ef245fdab763b7ccee9d36a7292c16db953e66a

Fix serious breakage introduced by #93
string-prefix-p doesn't work on symbols

* eglot.el (eglot-handle-notification): Coerce method name to string.

eglot.el
@@ -987,7 +987,7 @@ Uses THING, FACE, DEFS and PREPEND."
 (cl-defmethod eglot-handle-notification
   (_server method &key &allow-other-keys)
   "Handle unknown notification"
-  (unless (string-prefix-p "$" method)
+  (unless (string-prefix-p "$" (format "%s" method))
     (eglot--warn "Server sent unknown notification method `%s'" method)))
 
 (cl-defmethod eglot-handle-request