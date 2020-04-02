bab09f7f38f26eeebc2b37b2c84490fd5a71cc97
Yoni Rabkin <yoni@rabkins.net>
2020-04-02 13:50:02 -0400

    Upgrade to GPLv3

--- rt-liberation.el
+++ rt-liberation.el
@@ -1,4 +1,4 @@
-;;; rt-liberation.el --- Free from RT
+;;; rt-liberation.el --- Free from the RT Web Interface
 
 ;; Copyright (C) 2008, 2009, 2010, 2011, 2014, 2015 Yoni Rabkin, Aaron
 ;; S. Hawley, John Sullivan
@@ -8,7 +8,7 @@
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of the
+;; published by the Free Software Foundation; either version 3 of the
 ;; License, or (at your option) any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful,
