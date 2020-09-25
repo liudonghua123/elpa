984dc250333ee5efb6a166c0316cc498e382657e
Stefan Monnier <monnier@iro.umontreal.ca>
2020-09-25 11:10:53 -0400

    * shell-command+.el: Add copyright notice and change license to GPLv3+
    * .gitignore: New file.
    * LICENSE: Delete file.

--- shell-command+.el
+++ shell-command+.el
@@ -1,18 +1,25 @@
 ;;; shell-command+.el --- An extended shell-command -*- lexical-binding: t -*-
 
+;; Copyright (C) 2020  Free Software Foundation, Inc.
+
 ;; Author: Philip K. <philipk@posteo.net>
 ;; Version: 1.0.3
 ;; Keywords: unix, processes, convenience
 ;; Package-Requires: ((emacs "24.1"))
 ;; URL: http://elpa.gnu.org/packages/shell-command+.html
 
-;; This file is NOT part of Emacs.
-;;
-;; This file is in the public domain, to the extent possible under law,
-;; published under the CC0 1.0 Universal license.
-;;
-;; For a full copy of the CC0 license see
-;; https://creativecommons.org/publicdomain/zero/1.0/legalcode
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
