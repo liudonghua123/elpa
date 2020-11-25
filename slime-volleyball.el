7bca3e58acbe9e4a219cac54f91faa3bfc145a7d
Stefan Monnier <monnier@iro.umontreal.ca>
2020-11-24 21:15:11 -0500

    * slime-volleyball.el: Include the auxiliary files in the package

--- slime-volleyball.el
+++ slime-volleyball.el
@@ -6,6 +6,7 @@
 ;; Version: 1.1.5
 ;; Keywords: games
 ;; Package-Requires: ((cl-lib "0.5"))
+;; Package-Type: multi
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
