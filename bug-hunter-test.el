3eaaa92f981e034da6ea874420238a1b5058cce6
Stefan Monnier <monnier@iro.umontreal.ca>
2020-11-28 09:23:54 -0500

    * bug-hunter-test.el: Fix up copyright

--- bug-hunter-test.el
+++ bug-hunter-test.el
@@ -1,4 +1,27 @@
-(unless (bound-and-true-p package--initialized)
+;;; bug-hunter-test.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015-2020 Free Software Foundation, Inc.
+
+;; Author: Artur Malabarba <emacs@endlessparentheses.com>
+;; URL: https://github.com/Malabarba/elisp-bug-hunter
+;; Version: 1.3.1
+;; Keywords: lisp
+;; Package-Requires: ((seq "1.3") (cl-lib "0.5"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(unless (bound-and-true-p package--initialized)  ;; FIXME: Too lax!
   (setq
    package-user-dir (expand-file-name
                      (format ".cask/%s/elpa" emacs-version)
@@ -7,7 +30,7 @@
   (package-initialize))
 
 (require 'ert)
-(require 'cl)
+;; (require 'cl-lib)
 (require 'bug-hunter)
 ;; (fset 'bug-hunter--report #'ignore)
 ;; (fset 'bug-hunter--report-end #'ignore)
