cd290fc1cbafad328899082063130b1ddce1df1a
Philip Kaludercic <philipk@posteo.net>
2021-07-07 13:03:00 +0200

    Modify the local hook values in autocrypt-mu4e--uninstall

--- autocrypt-mu4e.el
+++ autocrypt-mu4e.el
@@ -30,8 +30,8 @@
 
 (defun autocrypt-mu4e--uninstall ()
   "Remove autocrypt hooks for mu4e."
-  (remove-hook 'mu4e-view-mode-hook #'autocrypt-process-header)
-  (remove-hook 'mu4e-compose-mode-hook #'autocrypt-compose-setup))
+  (remove-hook 'mu4e-view-mode-hook #'autocrypt-process-header t)
+  (remove-hook 'mu4e-compose-mode-hook #'autocrypt-compose-setup t))
 
 (defun autocrypt-mu4e--get-header (header)
   "Ask mu4e to return HEADER."
