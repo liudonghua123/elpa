7a04171296ecb5cfabbf5a86839189f6314abb2e
Tyler Grinn <tylergrinn@gmail.com>
2021-10-15 09:45:29 -0400

    Added more tooltip test cases

--- tests/boxy-test-visual.el
+++ tests/boxy-test-visual.el
@@ -1,4 +1,4 @@
-;;; boxy-test-chinese.Eli --- Visual tests for boxy -*- lexical-binding: t -*-
+;;; boxy-test-visual.el --- Visual tests for boxy -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2021 Free Software Foundation, Inc.
 
@@ -19,14 +19,26 @@
        (hello (boxy-box :name "你好" :rel "in" :tooltip "你好\n我叫泰勒")))
   (boxy-add-next greeting world)
   (boxy-add-next hello greeting)
-  (boxy-pp world))
+  (boxy-pp world :display-buffer-fn 'display-buffer-same-window))
 
 ;; Check that the tooltip for 你好 shows correctly
 (let* ((world (boxy-box))
        (greeting (boxy-box :name "我叫泰勒" :margin-y 0))
-       (hello (boxy-box :name "你好" :rel "in" :tooltip "你好\n我叫泰勒"))
-       (right (boxy-box :name "右边 " :rel "to the right of")))
+       (hello (boxy-box :name "你好" :rel "in" :tooltip "你好\n我叫泰勒我叫泰勒"))
+       (right (boxy-box :name "右边右边右边右边" :rel "to the right of")))
   (boxy-add-next greeting world)
   (boxy-add-next hello greeting)
   (boxy-add-next right greeting)
-  (boxy-pp world))
+  (boxy-pp world :display-buffer-fn 'display-buffer-same-window))
+
+
+;; Check that the tooltip for 你好 is truncated
+(let* ((boxy-tooltip-max-width 15)
+       (world (boxy-box))
+       (greeting (boxy-box :name "我叫泰勒" :margin-y 0))
+       (hello (boxy-box :name "你好" :rel "in" :tooltip "你好\n我叫泰勒我叫泰勒"))
+       (right (boxy-box :name "右边右边右边右边" :rel "to the right of")))
+  (boxy-add-next greeting world)
+  (boxy-add-next hello greeting)
+  (boxy-add-next right greeting)
+  (boxy-pp world :display-buffer-fn 'display-buffer-same-window))
