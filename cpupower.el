fa9888163dd28dc903286630d47de2c26f24f611
smeister <smeister@awstruepower.com>
2021-11-01 11:33:22 -0400

    more?

--- cpupower.el
+++ cpupower.el
@@ -1,5 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 
+;; Author: Stephen Meister
+;; URL   : https://gitlab.com/pallagun
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
