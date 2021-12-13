058dbe420e63bd460c0c8d09dd97ab55bc45533a
Antoine Kalmbach <ane@iki.fi>
2021-12-13 13:41:04 +0200

    set version to 1.8.2

--- rec-mode.el
+++ rec-mode.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Antoine Kalmbach <ane@iki.fi>
 ;; URL: https://www.gnu.org/software/recutils/
 ;; Package-Requires: ((emacs "25"))
-;; Version: 1.8.1
+;; Version: 1.8.2
 
 ;; This file is NOT part of GNU Emacs.
 
