1fd2e8df042f8e51a87173ce36220ee9a8c4241c
Thien-Thi Nguyen <ttn@gnuvola.org>
2021-12-19 04:38:34 -0500

    Release: 3.1.2
    * gnugo.el [Version]: Bump to "3.1.2".
    (gnugo-version): Bump to "3.1.2".

--- gnugo.el
+++ gnugo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
-;; Version: 3.1.1
+;; Version: 3.1.2
 ;; Package-Requires: ((ascii-art-to-unicode "1.5") (xpm "1.0.1") (cl-lib "0.5"))
 ;; Keywords: games, processes
 ;; URL: https://www.gnuvola.org/software/gnugo/
@@ -92,7 +92,7 @@
 ;;;---------------------------------------------------------------------------
 ;;; Political arts
 
-(defconst gnugo-version "3.1.1"
+(defconst gnugo-version "3.1.2"
   "Version of gnugo.el currently loaded.
 This follows a MAJOR.MINOR.PATCH scheme.")
 
