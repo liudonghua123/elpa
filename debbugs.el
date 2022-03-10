0be023a614f49f295e63337a875a7514db78be2f
Michael Albinus <michael.albinus@gmx.de>
2022-03-10 08:58:27 +0100

    Release debbugs 0.31

--- debbugs.el
+++ debbugs.el
@@ -5,7 +5,7 @@
 ;; Author: Michael Albinus <michael.albinus@gmx.de>
 ;; Keywords: comm, hypermedia
 ;; Package: debbugs
-;; Version: 0.30
+;; Version: 0.31
 ;; Package-Requires: ((emacs "25.1") (soap-client "3.1.5"))
 
 ;; This file is not part of GNU Emacs.
