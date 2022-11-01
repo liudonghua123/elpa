1932e3a42cf5cf2b4ea0ac0b8137556f7e656238
Stephen Leake <stephen_leake@stephe-leake.org>
2022-11-01 13:34:28 -0700

    Add files to gpr-mode

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Build executables for gpr mode.
+#    build.sh <other gprbuild options>
+#    e.g. 'build.sh -j0' : use all available processors to compile
+#         'build.sh -wn' : treat warnings as warnings.
+#         'build.sh -vh' : Verbose output (high verbosity)
+#
+# See install.sh for install
+
+if type alr; then
+    echo "building gpr-mode executables via Alire"
+    alr get emacs_gpr_mode~1.0.1
+    cd emacs_gpr_mode_*; alr build --release
+
+elif type gprbuild; then
+    echo "building gpr-mode executables via gnat compiler"
+    
+    if [ -d ../wisi-4.1.? ]; then
+        WISI_DIR=`ls -d ../wisi-4.1.?`
+    fi
+
+    args=`echo -DELPA="yes" $WISI_DIR/wisi.gpr.gp $WISI_DIR/wisi.gpr`
+    echo "gnatprep " $args
+    gnatprep $args
+
+    # We don't add WISI_DIR to GPR_PROJECT_PATH because the user may have
+    # already set GPR_PROJECT_PATH.
+
+    # Allow running build.sh again, since it often fails the first time.
+    #  - Run gprclean, to allow changing compilers and other drastic things
+
+    gprclean -r -P gpr_mode_wisi_parse.gpr -aP$WISI_DIR
+
+    gprbuild -p -j8 -P gpr_mode_wisi_parse.gpr -aP $WISI_DIR "$@"
+
+else
+    echo "neither Alire nor gnat compiler found"
+    return 1
+fi
+
+# end of file
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Install executables for Gpr mode.
+#
+# See build.sh for build (must be run before install).
+
+if type alr; then
+    echo "installing gpr-mode executables via Alire"
+
+    if [ x$1 == x ]; then
+        echo "you must specify the install directory with 'install.sh <dir>'"
+        return 1
+    fi
+    # IMPROVEME: when alr supports install, use that
+    cp emacs_gpr_mode*/bin/* $1
+
+elif type gprbuild; then
+    echo "installing gpr-mode executables via gnat compiler"
+    
+    # $1 : optional --prefix=<dir>
+    #
+    # If you don't have write permission in the GNAT installation
+    # directory, you need to use --prefix=<dir>, or run with root priviledges.
+
+    if [ -d ../wisi-4.1.? ]; then
+        WISI_DIR=`ls -d ../wisi-4.1.?`
+    fi
+    
+    gprinstall -f -p -P gpr_mode_wisi_parse.gpr -aP $WISI_DIR --install-name=gpr_mode_wisi_parse $1 
+
+else
+    echo "neither Alire nor gnat compiler found"
+    return 1
+fi
+
+# end of file.
