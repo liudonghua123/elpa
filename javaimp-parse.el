dcde3d417fcf9e2884f0c7314fdd248b7f4ca687
Stefan Kangas <stefankangas@gmail.com>
2022-12-21 09:03:14 +0100

    ; Prefer HTTPS to HTTP in URLs

--- javaimp-parse.el
+++ javaimp-parse.el
@@ -16,7 +16,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;; When changing parsing, it may make sense to benchmark performance
 ;; on a large source tree with something like this:
