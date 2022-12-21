775e236e48d917e40b8c3884c2774c45fdbca771
Stefan Kangas <stefankangas@gmail.com>
2022-12-21 09:05:10 +0100

    ; Prefer HTTPS to HTTP in URLs

--- markchars.el
+++ markchars.el
@@ -23,7 +23,7 @@
 ;; where they may be confused with regular chars. See `markchars-mode'
 ;; and `markchars-what'.  There are two modes: confusable detection
 ;; (where we look for mixed scripts within a word, without using the
-;; http://www.unicode.org/reports/tr39/ confusable tables) and pattern
+;; https://www.unicode.org/reports/tr39/ confusable tables) and pattern
 ;; detection (where any regular expressions can be matched).
 ;;
 ;; The marked text will have the 'markchars property set to either
