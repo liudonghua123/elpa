f46a0491f49775f30c50e1b1af355bb99a215d0e
Robert Brown <robert.brown@gmail.com>
2023-01-18 12:45:40 -0500

    Handling files whose names end in ".zip"
    This allows LSP servers to refer to source code in the JDK's src.zip
    file.
    
    Additionally, handle files in a jar or zip that do not have names
    containing a period character.
    
    Finally, update Java language server compatibility info in README.md.

--- jarchive.el
+++ jarchive.el
@@ -39,14 +39,14 @@
    line-start
    (or "jar:file://" "zipfile://")
    ;; match group 1, the jar file location
-   (group "/" (* not-newline) ".jar")
+   (group "/" (* not-newline) (or ".jar" ".zip"))
    ;; Delimiter between the jar and the file inside the jar
    (or "!" "::")
    ;; match the leading directory delimiter /,
-   ;; archvie mode expects none so it's outside match group 2
+   ;; archive mode expects none so it's outside match group 2
    (zero-or-one "/")
    ;; match group 2, the file within the archive
-   (group (* not-newline) "." (+ alphanumeric))
+   (group (* not-newline))
    line-end)
   "A regex for matching paths to a jar file and a file path into the jar file.
 Delimited by `!' for jar: schemes. Delimited by `::' for zipfile: schemes.")
