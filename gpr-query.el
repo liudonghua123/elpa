ada0d0afcefe88b66b9cc53de5d76db661bfb2d7
Stephen Leake <stephen_leake@stephe-leake.org>
2023-01-28 11:20:55 -0800

    Release 1.0.3
    * gpr-query.el: Bump version.

--- gpr-query.el
+++ gpr-query.el
@@ -7,8 +7,8 @@
 
 ;; Author: Stephen Leake <stephen_leake@member.fsf.org>
 ;; Maintainer: Stephen Leake <stephen_leake@member.fsf.org>
-;; Version: 1.0.2
-;; package-requires: ((emacs "25.3") (wisi "4.2.0") (gnat-compiler "1.0.1"))
+;; Version: 1.0.3
+;; package-requires: ((emacs "25.3") (wisi "4.2.2") (gnat-compiler "1.0.2"))
 
 ;; This file is part of GNU Emacs.
 
