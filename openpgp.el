9385b7502edd5374073f6492b8a05f8ad6116d6c
Philip Kaludercic <philip.kaludercic@fau.de>
2023-03-20 23:02:35 +0000

    Format file as a proper Emacs package

--- openpgp.el
+++ openpgp.el
@@ -1,6 +1,32 @@
-;;; $Id: openpgp.el,v 1.15 2020/03/05 13:45:21 oj14ozun Exp oj14ozun $
-;;; Implementation of the keys.openpgp.org protocol as specified by
-;;; https://keys.openpgp.org/about/api
+;;; openpgp.el --- Client for keys.openpgp.org       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Philip Kaludercic
+
+;; Author: Philip Kaludercic
+;; Keywords: mail
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A basic client for querying, verifying and uploading keys to the
+;; keys.openpgp.org keyserver.
+;;
+;; After loading the file, use C-h a openpgp- RET to generate a list
+;; of entry points.
+
+;;; Code:
 
 (require 'json)
 (require 'url)
@@ -171,3 +197,6 @@ key."
 		       (mu4e-message-field msg :from))))
 	(when (yes-or-no-p (format "Attempt to fetch key for %s? " email))
 	  (openpgp-fetch-key-by-email email))))))
+
+(provide 'openpgp)
+;;; openpgp.el ends here
