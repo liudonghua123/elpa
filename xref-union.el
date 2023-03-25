eefdc0b2c0d44789dc22607e47755bc182d66839
Philip Kaludercic <philipk@posteo.net>
2023-03-25 14:31:35 +0100

    Assign copyright to the FSF

--- xref-union.el
+++ xref-union.el
@@ -1,6 +1,6 @@
 ;;; xref-union.el --- Combine multiple Xref backends  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Philip Kaludercic
+;; Copyright (C) 2023  Free Software Foundation, Inc.
 
 ;; Author: Philip Kaludercic <philipk@posteo.net>
 ;; Maintainer: Philip Kaludercic <~pkal/public-inbox@lists.sr.ht>
