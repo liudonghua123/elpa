557cf93559e2d376cb792f7a63af8683726b044b
Stefan Monnier <monnier@iro.umontreal.ca>
2023-05-01 18:30:17 -0400

    * hyperbole.el: Preload `kotl-autoloads`
    (hyperbole-mode): Remove redundant keywords.

--- hyperbole.el
+++ hyperbole.el
@@ -78,6 +78,9 @@
 ;;; Start Initializations
 ;;; ************************************************************************
 
+;; FIXME: Just loading `hyperbole-autoloads.el' eagerly loads `hhist.el',
+;; `set.el', and `hact.el'.
+
 (defconst hyperbole-loading t
   "Temporary constant available for testing while Hyperbole is loading.")
 
@@ -559,6 +562,19 @@ frame, those functions by default still return the prior frame."
 (hyperbole-mode 1)
 
 (makunbound 'hyperbole-loading)
+
+;; Autoload this form so that when `package.el' activates Hyperbole's autoloads
+;; it also sets up Kotl's autoloads.
+;;;###autoload
+(let ((us (if (fboundp 'macroexp-file-name)
+              (macroexp-file-name) load-file-name)))
+  (when us
+    ;; FIXME: Contrary to the usual ELPA autoloads files, `kotl-autoloads'
+    ;; does not add its directory to `load-path', so let's do it here by
+    ;; hand.
+    (add-to-list 'load-path
+                 (expand-file-name "kotl" (file-name-directory us)))
+    (require 'kotl-autoloads nil t)))
 
 (provide 'hyperbole)
 
