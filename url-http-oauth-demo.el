17230fc6513df52f1417b335f64c9f405e5a9126
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2023-05-08 22:05:18 -0400

    Bump version to 1.0.1
    * url-http-oauth-demo.el, NEWS: Bump version to 1.0.1.

--- url-http-oauth-demo.el
+++ url-http-oauth-demo.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023 Free Software Foundation, Inc.
 
 ;; Author: Thomas Fitzsimmons <fitzsim@fitzsim.org>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: comm, data, processes, hypermedia
 ;; Package-Requires: ((url-http-oauth "0.8.1"))
 
