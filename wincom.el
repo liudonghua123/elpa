f1693950a97c903c3441f454a08490fe35f4eb2f
Daniel Semyonov <daniel@dsemy.com>
2023-06-28 20:55:03 +0300

    ; Bump version to 3.0

--- wincom.el
+++ wincom.el
@@ -4,7 +4,7 @@
 
 ;; Author: Daniel Semyonov <daniel@dsemy.com>
 ;; Maintainer: Daniel Semyonov <daniel@dsemy.com>
-;; Version: 2.3
+;; Version: 3.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience
 ;; URL: https://dsemy.com/projects/window-commander
