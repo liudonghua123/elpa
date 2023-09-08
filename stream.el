5c0ffd088fbc344aedb11cc76bd7c0b28f3bc43b
Philip Kaludercic <philipk@posteo.net>
2023-09-08 09:44:47 +0200

    * packages/stream/stream.el: Bump version to 2.3.0

--- stream.el
+++ stream.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nicolas Petton <nicolas@petton.fr>
 ;; Keywords: stream, laziness, sequences
-;; Version: 2.2.5
+;; Version: 2.3.0
 ;; Package-Requires: ((emacs "25"))
 ;; Package: stream
 
